f686d86ccab074870758a4dceeb213456cd1adff
Tiago Gomes <tiagogomes187@gmail.com>
2024-01-02 22:03:37 -0300

    First class: Position

---
+++ src/br/dev/tiagogomes/application/Program.java
@@ -0,0 +1,12 @@
+package br.dev.tiagogomes.application;
+
+import br.dev.tiagogomes.boardgame.Position;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Position pos = new Position(3, 5);
+
+        System.out.println(pos);
+    }
+}
