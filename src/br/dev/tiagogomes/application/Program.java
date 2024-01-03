0c4afa583d198a14ab82794f40ea255bf31e3c51
Tiago Gomes <tiagogomes187@gmail.com>
2024-01-02 23:03:34 -0300

    Starting to implement Board and Piece

--- src/br/dev/tiagogomes/application/Program.java
+++ src/br/dev/tiagogomes/application/Program.java
@@ -1,12 +1,13 @@
 package br.dev.tiagogomes.application;
 
+import br.dev.tiagogomes.boardgame.Board;
 import br.dev.tiagogomes.boardgame.Position;
 
 public class Program {
     public static void main(String[] args) {
 
-        Position pos = new Position(3, 5);
+        Board board = new Board(8,8);
 
-        System.out.println(pos);
+
     }
 }
---
+++ src/br/dev/tiagogomes/boardgame/Piece.java
@@ -0,0 +1,18 @@
+package br.dev.tiagogomes.boardgame;
+
+public class Piece {
+
+    protected Position position;
+    private Board board;
+
+    public Piece(Board board) {
+        this.board = board;
+        position = null;
+    }
+
+    protected Board getBoard() {
+        return board;
+    }
+
+
+}
